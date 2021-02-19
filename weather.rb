260238d4f2ff6e1e3dffc4dae6cf86ea322d1120
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-18 21:00:14 -0500

    make dynamic city

--- weather.rb
+++ weather.rb
@@ -1,12 +1,12 @@
 require "http"
+p "Please enter a city"
+input_city = gets.chomp
 
-response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=brooklyn&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
-
-# pp response.parse
+response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{input_city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
 
 temp_max = response.parse["main"]["temp_max"]
 temp_min = response.parse["main"]["temp_min"]
-p response.parse["main"]["feels_like"]
-p response.parse["weather"][0]["description"]
+feels_like = response.parse["main"]["feels_like"]
+description = response.parse["weather"][0]["description"]
 
-p "Today in Brooklyn the temperature high was #{temp_max}"
+p "Today, in #{input_city.capitalize} the temperature high was #{temp_max}, the low was #{temp_min}, the real feel was #{feels_like} overhead were #{description}"
