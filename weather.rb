b84383396f7a4bdf68ef3ffa77f8c07dec6019ae
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-06 16:25:34 -0400

    fix response

--- weather.rb
+++ weather.rb
@@ -2,11 +2,12 @@ require "http"
 p "Please enter a city"
 input_city = gets.chomp
 
-response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{input_city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{input_city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial").to_s
+data = JSON.parse(response)
 
-temp_max = response.parse["main"]["temp_max"]
-temp_min = response.parse["main"]["temp_min"]
-feels_like = response.parse["main"]["feels_like"]
-description = response.parse["weather"][0]["description"]
+temp_max = data["main"]["temp_max"]
+temp_min = data["main"]["temp_min"]
+feels_like = data["main"]["feels_like"]
+description = data["weather"][0]["description"]
 
-p "Today, in #{input_city.capitalize} the temperature high was #{temp_max}, the low was #{temp_min}, the real feel was #{feels_like} overhead were #{description}"
+p "Today, in #{input_city.capitalize} the temperature high was #{temp_max}, the low was #{temp_min}. Right now in #{input_city.capitalize} it feels like #{feels_like} overhead were #{description}"
